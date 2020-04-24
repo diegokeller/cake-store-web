dd482205df3c01250e43df3497caafeed591aacc
Diego Keller <keller.diego@gmail.com>
2020-04-23 23:19:47 -0300

    Ajustes no detalhe do produto

--- src/app/cake-store-api.service.ts
+++ src/app/cake-store-api.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from './../environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,11 +10,11 @@ export class CakeStoreApiService {
 
   constructor(private http: HttpClient) { }
 
-  getProdutos() {
+  getProdutos(): Observable<any> {
     return this.http.get(`${environment.apiUrl}/produtos.json`);
   }
 
-  getProduto(id) {
+  getProduto(id): Observable<any> {
     return this.http.get(`${environment.apiUrl}/produtos/${id}.json`);
   }
 
